87ac98583ea7ec4e99ab44338a66a2308252d6f2
m-root <muemakamba@gmail.com>
2019-09-19 08:35:51 +0300

    Update
    Update

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ MODEL, LABELS, LABELS_DICT = get_model()
 @app.route('/api/validateSymptom', methods = ['POST'])
 def getCorrectSymptomName():
   data = request.get_json(silent=True)
-  name = escape(data.get('name')).lower()
+  name = escape(data('name')).lower()
   if name in nameToCUI:
     return jsonify({nameToCUI[name]: name})
   else:
